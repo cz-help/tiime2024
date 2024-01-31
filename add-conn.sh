a18192113ae390da6e72f204c1ce6621f1f1c09d
Kamil Jires <kamil@jires.eu>
2024-02-01 00:02:36 +0100

    clean up and small correction in the files...

--- add-conn.sh
+++ add-conn.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -v $(basename $(pwd))_midpoint_home:/opt/midpoint/var -v ./${1}:/tmp/${1} evolveum/midpoint:${MP_VER:-latest}-alpine bash -c "ls -la /opt/midpoint/var/connid-connectors ; echo ' - - - - - - - -'; cp /tmp/${1} /opt/midpoint/var/connid-connectors ; ls -la /opt/midpoint/var/connid-connectors; "
+docker run --rm -v $(basename $(pwd))_midpoint_home:/opt/midpoint/var -v ./connector-ssh-1.0.jar:/tmp/connector-ssh-1.0.jar evolveum/midpoint:${MP_VER:-latest}-alpine bash -c "ls -la /opt/midpoint/var/connid-connectors ; echo ' - - - - - - - -'; cp /tmp/connector-ssh-1.0.jar /opt/midpoint/var/connid-connectors ; ls -la /opt/midpoint/var/connid-connectors; "
